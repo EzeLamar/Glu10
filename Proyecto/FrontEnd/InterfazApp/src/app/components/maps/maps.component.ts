7c685a94673cec04c2de96c0be71d04db7187546
EzeLamar <eze93mdoz@gmail.com>
2018-10-17 13:29:04 -0300

    se agregaro menu para cambiar radio (no funciona la logica todav)

--- Proyecto/FrontEnd/InterfazApp/src/app/components/maps/maps.component.ts
+++ Proyecto/FrontEnd/InterfazApp/src/app/components/maps/maps.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
+
 
 @Component({
   selector: 'app-maps',
@@ -7,12 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  marcadores: Marcador[] = [];
 
-  constructor() { }
+  // lat: number = 51.678418;
+  // lng: number = 7.809007;
+  lat: number = -38.71536909404415;
+  lng: number = -62.26685779187005;
+
+  constructor() { 
+    if(localStorage.getItem('marcadores')){  //si existe
+      this.marcadores = JSON.parse( localStorage.getItem('marcadores') );
+    } 
+    else{
+      const nuevoMarcador = new Marcador(this.lat,this.lng );
+      this.marcadores.push(nuevoMarcador);
+    }
+  }
 
   ngOnInit() {
+  }
+
+  agregarMarcador( evento ){
+    const coords: { lat: number, lng: number } = evento.coords;
+
+    const nuevoMarcador = new Marcador(coords.lat, coords.lng);
+    nuevoMarcador.titulo = "Hola";
+    nuevoMarcador.desc= "este es un lugar copado";
+     
+
+    this.marcadores.push(nuevoMarcador);
+    this.guardaMarcadores();
+
+  }
+
+  guardaMarcadores(){
+    localStorage.setItem('marcadores',JSON.stringify(this.marcadores));
   }
 
 }
