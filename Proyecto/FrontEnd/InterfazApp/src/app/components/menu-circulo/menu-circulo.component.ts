86a49b08897344abe41da805edbfeae557f28356
EzeLamar <eze93mdoz@gmail.com>
2018-10-17 13:27:14 -0300

    se agrego circulo sobre el mapa

---
+++ Proyecto/FrontEnd/InterfazApp/src/app/components/menu-circulo/menu-circulo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-circulo',
+  templateUrl: './menu-circulo.component.html',
+  styleUrls: ['./menu-circulo.component.css']
+})
+export class MenuCirculoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
