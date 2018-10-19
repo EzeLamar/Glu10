3031b53e07c1b01b270278f404531f02031236ca
EzeLamar <eze93mdoz@gmail.com>
2018-10-19 11:53:35 -0300

    se agrego y configuró y usó el nuevo servicio MarcadorService

--- Proyecto/FrontEnd/InterfazApp/src/app/components/maps/maps.component.ts
+++ Proyecto/FrontEnd/InterfazApp/src/app/components/maps/maps.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Marcador } from '../../classes/marcador.class';
 
+//serivcio importado
+import { MarcadoresService } from "../../services/marcadores.service";
+
 
 @Component({
   selector: 'app-maps',
@@ -9,36 +12,41 @@ import { Marcador } from '../../classes/marcador.class';
 })
 export class MapsComponent implements OnInit {
 
-  marcadores: Marcador[] = [];
-
-  // lat: number = 51.678418;
-  // lng: number = 7.809007;
+  //para pruebas
   lat: number = -38.71536909404415;
   lng: number = -62.26685779187005;
 
-  constructor() { 
-    if(localStorage.getItem('marcadores')){  //si existe
-      this.marcadores = JSON.parse( localStorage.getItem('marcadores') );
-    } 
-    else{
-      const nuevoMarcador = new Marcador(this.lat,this.lng );
-      this.marcadores.push(nuevoMarcador);
-    }
+  //atributos de la clase
+  marcadores: Marcador[] = [];
+  error = 'todo bien';
+
+  constructor( private marcadorService: MarcadoresService ) { 
+    // if(localStorage.getItem('marcadores')){  //si existe
+    //   this.marcadores = JSON.parse( localStorage.getItem('marcadores') );
+    // } 
+    // else{
+    //   const nuevoMarcador = new Marcador(this.lat,this.lng );
+    //   this.marcadores.push(nuevoMarcador);
+    // }
   }
 
   ngOnInit() {
+    console.log(this.error);
+    this.obtenerMarcadoresServer();
+    console.log(this.error);
+    //this.obtenerPrueba();
   }
 
   agregarMarcador( evento ){
     const coords: { lat: number, lng: number } = evento.coords;
 
+    console.log("lat:"+coords.lat+", long:"+coords.lng);
     const nuevoMarcador = new Marcador(coords.lat, coords.lng);
-    nuevoMarcador.titulo = "Hola";
-    nuevoMarcador.desc= "este es un lugar copado";
-     
-
+    nuevoMarcador.nombre = "Hola";
+    nuevoMarcador.descripcion= "este es un lugar copado";
     this.marcadores.push(nuevoMarcador);
-    this.guardaMarcadores();
+    
+    //this.guardaMarcadores();
 
   }
 
@@ -46,4 +54,25 @@ export class MapsComponent implements OnInit {
     localStorage.setItem('marcadores',JSON.stringify(this.marcadores));
   }
 
+  obtenerMarcadoresServer(): void{
+    this.marcadorService.getAll().subscribe(
+      ( res: Marcador[] ) => {
+        this.marcadores = res;
+      },
+      ( err ) => {
+        this.error= err;   //VER DSPS: nunca recibe el mensaje de error , por loque nunca cambia. 
+      }
+    );
+  }
+
+  obtenerPrueba(): void{
+    this.marcadorService.getPrueba().subscribe(
+      ( res: string ) => {
+        console.log("Component:"+res);
+      },
+      ( err ) => {
+        this.error= err;   //VER DSPS: nunca recibe el mensaje de error , por loque nunca cambia. 
+      }
+    );
+  }
 }
