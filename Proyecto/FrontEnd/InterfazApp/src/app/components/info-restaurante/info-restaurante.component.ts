e77f3a11d1a83c110eaa2da54ca35728d486f021
EzeLamar <eze93mdoz@gmail.com>
2018-11-13 17:08:27 -0300

    se agrego componente para informacion detallada de restaurante (VerMas)

---
+++ Proyecto/FrontEnd/InterfazApp/src/app/components/info-restaurante/info-restaurante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-restaurante',
+  templateUrl: './info-restaurante.component.html',
+  styleUrls: ['./info-restaurante.component.css']
+})
+export class InfoRestauranteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
